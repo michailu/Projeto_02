eb009ddb261d372ae9a3ece23d1b31ede47bff88
michailu <michailu@gmail.com>
2019-07-16 19:53:58 -0300

    Commit inicial

---
+++ src/TestaCaracteres.java
@@ -0,0 +1,11 @@
+
+public class TestaCaracteres {
+
+	public static void main(String[] args) {
+		
+		char letra = '#';
+		
+		System.out.println(letra);
+	}
+
+}
---
+++ src/TestaVariaveis.java
@@ -0,0 +1,41 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 42;
+		
+		double divisao = 5.0 / 2;
+		
+		System.out.println("Idade "+idade);
+		
+		System.out.println("Divisao "+divisao);
+		
+		System.out.println("Soma "+ (idade + divisao));
+
+		double novaDivisao = 5 / 2; //Calculo com inteiro o resultado sera inteiro
+		
+		System.out.println(novaDivisao);
+		
+		int novaTentativa = 5 / 2;
+		
+		System.out.println(novaTentativa);
+		
+		double salario = 1050.7;
+		
+		int salarioInt = (int) salario;
+		
+		System.out.println(salarioInt);
+		
+		long numeroLong = 1234555444444444449l;
+		
+		short numeroShort = 32767;
+		
+		byte numeroByte = 127;
+		
+		float numeroFloat = 949f;
+		
+		System.out.println("Teste maximo dos tipos: " + numeroFloat);
+	}
+
+}
