69c27801a3df9545eaa6d96895c88bb8b421502e
michailu <michailu@gmail.com>
2019-07-23 12:52:25 -0300

    Alterado arquivo excluido no github

--- src/TestaCaracteres.java
+++ src/TestaCaracteres.java
@@ -3,7 +3,7 @@ public class TestaCaracteres {
 
 	public static void main(String[] args) {
 		char letra = '#';
-		System.out.println(letra); // Comentario adicionado pelo Eclipse
+		System.out.println(letra); // Alterado arquivo que foi excluido pelo github
 		char valor = 1;
 		System.out.println(valor);
 	}
